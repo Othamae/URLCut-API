da0eb89b4bff68347c7da7e0c9244db6d7e0d167
Othamae <vero.gconesa@gmail.com>
2024-01-29 18:35:56 +0100

    feat: :tada: First commit

---
+++ src/main/java/dev/othamae/urlcorto/UrlcortoApplication.java
@@ -0,0 +1,13 @@
+package dev.othamae.urlcorto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlcortoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlcortoApplication.class, args);
+	}
+
+}
