46000f16cde619950d02a3b49e8c8a6a0545220e
Othamae <vero.gconesa@gmail.com>
2024-01-29 18:25:40 +0100

    docs: :memo: Added Swagger config

---
+++ src/main/java/dev/othamae/urlcorto/config/OpenAPIConfig.java
@@ -0,0 +1,44 @@
+package dev.othamae.urlcorto.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Value("http://localhost:8080")
+    private String devUrl;
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("URLCorto API")
+                        .description("URL shortener API")
+                        .version("v1.0")
+                        .contact(new Contact()
+                                .name("Othamae")
+                                .email("info@othamae.dev")
+                                .url("othamae.dev")));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
